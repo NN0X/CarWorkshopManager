a955d382561d230ab8e5e9692fbd1fa73c690f05
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-06 20:50:48 +0200

    Add OrderStatus model

---
+++ CarWorkshopManager/Models/Domain/OrderStatus.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarWorkshopManager.Models.Domain;
+
+public class OrderStatus
+{
+    public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; } = string.Empty;
+}
