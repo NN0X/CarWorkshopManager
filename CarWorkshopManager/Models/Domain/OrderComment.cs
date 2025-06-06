e2046e30bcb18cc17c53a8eeb59d31824733ec2a
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-06 23:14:37 +0200

    Add OrderComment model

---
+++ CarWorkshopManager/Models/Domain/OrderComment.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CarWorkshopManager.Models.Identity;
+
+namespace CarWorkshopManager.Models.Domain;
+
+public class OrderComment
+{
+    public int Id { get; set; }
+    
+    [Required]
+    public int ServiceOrderId { get; set; }
+
+    [Required] 
+    public string AuthorId { get; set; } = string.Empty;
+    
+    [Required]
+    [MaxLength(1000)]
+    public string Content { get; set; } = string.Empty;
+    
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    
+    public ServiceOrder ServiceOrder { get; set; } = null!;
+    public ApplicationUser Author { get; set; } = null!;
+}
