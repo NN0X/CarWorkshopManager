119744171ea22505d6d386277bdb597772f129e5
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-06 20:42:49 +0200

    Add VatRate model

---
+++ CarWorkshopManager/Models/Domain/VatRate.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarWorkshopManager.Models.Domain;
+
+public class VatRate
+{
+    public int Id { get; set; }
+    
+    [Required]
+    [Precision(4, 2)]
+    [Range(0.00, 1.00)]
+    public decimal Rate { get; set; }
+    
+    [Required]
+    [MaxLength(15)]
+    public string Code { get; set; } = string.Empty;
+    
+    [Required]
+    public DateOnly ValidFrom { get; set; }
+    
+    public DateOnly? ValidTo { get; set; }
+}
