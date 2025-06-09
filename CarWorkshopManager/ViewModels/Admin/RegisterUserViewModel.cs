55ace594400c4a054b56d4a04912501014d329ca
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-09 20:06:54 +0200

    [US1] Add ViewModels for Login, ResetPassword and Register

---
+++ CarWorkshopManager/ViewModels/Admin/RegisterUserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarWorkshopManager.ViewModels.Admin;
+
+public class RegisterUserViewModel
+{
+    [Required]
+    [Display(Name = "Imię")]
+    public string FirstName { get; set; } = string.Empty;
+    
+    [Required]
+    [Display(Name = "Nazwisko")]
+    public string LastName { get; set; } = string.Empty;
+    
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    
+    [Phone]
+    [Display(Name = "Numer telefonu")]
+    public string? PhoneNumber { get; set; }
+    
+    [Required]
+    [Display(Name = "Rola")]
+    public string Role { get; set; } = string.Empty;
+}
