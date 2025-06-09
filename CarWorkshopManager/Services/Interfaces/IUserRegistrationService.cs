2b14813b790d4ddaa440cce1d370aa277ee98fc1
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-09 19:04:43 +0200

    [US1] Add UserRegistrationService

---
+++ CarWorkshopManager/Services/Interfaces/IUserRegistrationService.cs
@@ -0,0 +1,10 @@
+﻿using CarWorkshopManager.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace CarWorkshopManager.Services.Interfaces;
+
+public interface IUserRegistrationService
+{
+    Task<(IdentityResult result, ApplicationUser user, string token)>
+        RegisterUserAsync(string firstName, string lastName, string email, string? phoneNumber, string role);
+}
