11a763862343087cfca078a46110447f518101d1
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-09 20:06:54 +0200

    [US1] Add ViewModels for Login, ResetPassword and Register

---
+++ CarWorkshopManager/ViewModels/Account/ResetPasswordViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarWorkshopManager.ViewModels.Account;
+
+public class ResetPasswordViewModel
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    
+    [Required]
+    public string Token { get; set; } = string.Empty;
+    
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Nowe hasło")]
+    public string Password { get; set; } = string.Empty;
+    
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Potwierdź Hasło")]
+    [Compare("Password", ErrorMessage = "Hasła nie są jednakowe.")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
