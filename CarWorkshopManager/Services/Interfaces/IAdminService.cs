a956872db9a2ea5e18b4c1c9b7a389c33c408546
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-10 03:13:55 +0200

    [US2] Add AdminService

---
+++ CarWorkshopManager/Services/Interfaces/IAdminService.cs
@@ -0,0 +1,9 @@
+﻿using CarWorkshopManager.ViewModels.Admin;
+
+namespace CarWorkshopManager.Services.Interfaces;
+
+public interface IAdminService
+{
+    Task<List<UserListItemViewModel>> GetAllUsersAsync();
+    Task<bool> ChangeUserRoleAsync(string userId, string newRole);
+}
