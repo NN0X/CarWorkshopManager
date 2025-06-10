c2709966a2a6e4551955466d16616f774f9d433e
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-10 13:12:25 +0200

    [US3] Add Customer service (+ interface)

---
+++ CarWorkshopManager/Services/Implementations/CustomerService.cs
@@ -0,0 +1,32 @@
+﻿using CarWorkshopManager.Data;
+using CarWorkshopManager.Mappers;
+using CarWorkshopManager.Services.Interfaces;
+using CarWorkshopManager.ViewModels.Customer;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarWorkshopManager.Services.Implementations;
+
+public class CustomerService : ICustomerService
+{
+    private readonly ApplicationDbContext _db;
+    private readonly CustomerMapper _mapper;
+
+    public CustomerService(ApplicationDbContext db, CustomerMapper mapper)
+    {
+        _db = db;
+        _mapper = mapper;
+    }
+
+    public async Task AddCustomerAsync(CreateCustomerViewModel model)
+    {
+        var customer = _mapper.ToCustomer(model);
+        _db.Customers.Add(customer);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task<List<CustomerListItemViewModel>> GetAllCustomersAsync()
+    {
+        var customers = await _db.Customers.OrderBy(c => c.Id).ToListAsync();
+        return customers.Select(_mapper.ToCreateCustomerListItemViewModel).ToList();
+    }
+}
