d7de05e38a11eefa245a310a99924ac6c48374b4
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-10 03:13:55 +0200

    [US2] Add AdminService

---
+++ CarWorkshopManager/Services/Implementations/AdminService.cs
@@ -0,0 +1,60 @@
+﻿using CarWorkshopManager.Constants;
+using CarWorkshopManager.Models.Identity;
+using CarWorkshopManager.Services.Interfaces;
+using CarWorkshopManager.ViewModels.Admin;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarWorkshopManager.Services.Implementations;
+
+public class AdminService : IAdminService
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public AdminService(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<List<UserListItemViewModel>> GetAllUsersAsync()
+    {
+        var users = await _userManager.Users.ToListAsync();
+        
+        var result = new List<UserListItemViewModel>();
+
+        foreach (var user in users)
+        {
+            var role = (await _userManager.GetRolesAsync(user)).FirstOrDefault() ?? "Brak";
+            
+            result.Add(new UserListItemViewModel
+            {
+                Id = user.Id,
+                Username = user.UserName ?? "",
+                FullName = $"{user.FirstName} {user.LastName}",
+                Email = user.Email ?? "",
+                PhoneNumber = user.PhoneNumber ?? "",
+                Role = role
+            });
+        }
+
+        return result;
+    }
+
+    public async Task<bool> ChangeUserRoleAsync(string userId, string newRole)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user is null)
+            return false;
+        
+        var currentRoles = await _userManager.GetRolesAsync(user);
+        if (currentRoles.Any())
+        {
+            var removeResult = await _userManager.RemoveFromRolesAsync(user, currentRoles);
+            if (!removeResult.Succeeded)
+                return false;
+        }
+        
+        var result = await _userManager.AddToRoleAsync(user, newRole);
+        return result.Succeeded;
+    }
+}
