eedf88b3317f457b1d4d3efadd0694939c5e7e93
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-10 13:10:38 +0200

    [US3] Add Customer mapper

---
+++ CarWorkshopManager/Mappers/CustomerMapper.cs
@@ -0,0 +1,12 @@
+﻿using CarWorkshopManager.Models.Domain;
+using CarWorkshopManager.ViewModels.Customer;
+using Riok.Mapperly.Abstractions;
+
+namespace CarWorkshopManager.Mappers;
+
+[Mapper(RequiredMappingStrategy = RequiredMappingStrategy.None)]
+public partial class CustomerMapper
+{
+    public partial Customer ToCustomer(CreateCustomerViewModel model);
+    public partial CustomerListItemViewModel ToCreateCustomerListItemViewModel(Customer customer);
+}
