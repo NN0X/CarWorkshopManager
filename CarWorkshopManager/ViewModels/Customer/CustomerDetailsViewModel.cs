9503ded81dfaa4a52a581a9a21b0960a7da50895
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-10 16:44:12 +0200

    [US3/4] Add Customer Details ViewModel.

---
+++ CarWorkshopManager/ViewModels/Customer/CustomerDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CarWorkshopManager.ViewModels.Vehicle;
+
+namespace CarWorkshopManager.ViewModels.Customer;
+
+public class CustomerDetailsViewModel
+{
+    public int Id { get; set; }
+    public string FirstName   { get; set; } = string.Empty;
+    public string LastName    { get; set; } = string.Empty;
+    public string Email       { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public List<VehicleListItemViewModel> Vehicles { get; set; } = new();
+}
