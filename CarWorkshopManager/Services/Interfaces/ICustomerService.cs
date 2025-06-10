d3fd5b0f0592b73f67acceb5d84810742d2ab2b6
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-10 13:12:25 +0200

    [US3] Add Customer service (+ interface)

---
+++ CarWorkshopManager/Services/Interfaces/ICustomerService.cs
@@ -0,0 +1,9 @@
+﻿using CarWorkshopManager.ViewModels.Customer;
+
+namespace CarWorkshopManager.Services.Interfaces;
+
+public interface ICustomerService
+{
+    Task AddCustomerAsync(CreateCustomerViewModel model);
+    Task<List<CustomerListItemViewModel>> GetAllCustomersAsync();
+}
