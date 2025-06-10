48c8186d6b63ef4902cb6ee577ff6b33fbb2e9f5
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-10 16:50:38 +0200

    [US4] Add VehicleController

---
+++ CarWorkshopManager/Controllers/VehicleController.cs
@@ -0,0 +1,31 @@
+﻿using CarWorkshopManager.Services.Interfaces;
+using CarWorkshopManager.ViewModels.Vehicle;
+using Microsoft.AspNetCore.Mvc;
+
+public class VehicleController : Controller
+{
+    private readonly IVehicleService _vehicleService;
+
+    public VehicleController(IVehicleService vehicleService)
+    {
+        _vehicleService = vehicleService;
+    }
+
+    [HttpGet]
+    public IActionResult Add(int customerId)
+    {
+        return View(new AddVehicleViewModel { CustomerId = customerId });
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Add(AddVehicleViewModel vm)
+    {
+        if (!ModelState.IsValid) return View(vm);
+
+        await _vehicleService.AddVehicleAsync(vm);
+        TempData["Success"] = "Pojazd dodany.";
+
+        return RedirectToAction("Details", "Customer", new { id = vm.CustomerId });
+    }
+}
