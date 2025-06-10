503f4442d1328d3ad9ad55324a5ef23d6f2c8b8a
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-10 13:09:42 +0200

    [US3] Add Customer ViewModels for reading and create

---
+++ CarWorkshopManager/ViewModels/Customer/CreateCustomerViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarWorkshopManager.ViewModels.Customer;
+
+public class CreateCustomerViewModel
+{
+    [Required] 
+    [MaxLength(200)] 
+    [Display(Name = "Imię")]
+    public string FirstName { get; set; } = string.Empty;
+    
+    [Required]
+    [MaxLength(200)]
+    [Display(Name = "Nazwisko")]
+    public string LastName { get; set; } = string.Empty;
+    
+    [Required]
+    [EmailAddress]
+    [MaxLength(200)]
+    public string Email { get; set; } = string.Empty;
+    
+    [Required]
+    [Phone]
+    [MaxLength(20)]
+    [Display(Name = "Numer telefonu")]
+    public string PhoneNumber { get; set; } = string.Empty;
+}
