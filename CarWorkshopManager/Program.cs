6e6dacabf47cfc93206c8a876367daf158411dc7
nox <nox@noxsoftware.pl>
2025-06-10 21:52:00 +0200

    test fixes

--- CarWorkshopManager/Program.cs
+++ CarWorkshopManager/Program.cs
@@ -9,7 +9,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -44,11 +43,9 @@ using (var scope = app.Services.CreateScope())
     await IdentitySeeder.SeedAsync(services);
 }
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -62,6 +59,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 
 app.Run();
