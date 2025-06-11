a165545b4e6480803e7be91bff500ae4655b05ee
Grzegorz Rzepiela <grzegorzrzepiela5@gmail.com>
2025-06-11 18:59:04 +0200

    [US6] Develop ServiceOrderService (+interface)

---
+++ CarWorkshopManager/Services/Interfaces/IServiceOrderService.cs
@@ -0,0 +1,8 @@
+﻿using CarWorkshopManager.ViewModels.ServiceOrder;
+
+namespace CarWorkshopManager.Services.Interfaces;
+
+public interface IServiceOrderService
+{
+    Task<int> CreateOrderAsync(CreateServiceOrderViewModel createServiceOrderViewModel, string userId);
+}
