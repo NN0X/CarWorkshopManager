5b74103bc747c7e10849f48d21db158087739c54
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-11 22:45:55 +0200

    Add PartService (+interface)

---
+++ CarWorkshopManager/Services/Implementations/PartService.cs
@@ -0,0 +1,46 @@
+﻿using CarWorkshopManager.Data;
+using CarWorkshopManager.Mappers;
+using CarWorkshopManager.Services.Interfaces;
+using CarWorkshopManager.ViewModels.Part;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarWorkshopManager.Services.Implementations;
+
+public class PartService : IPartService
+{
+    private readonly ApplicationDbContext _db;
+    private readonly PartMapper _mapper;
+
+    public PartService(ApplicationDbContext db, PartMapper mapper)
+    {
+        _db = db;
+        _mapper = mapper;
+    }
+
+    public async Task<List<PartListItemViewModel>> GetAllPartsAsync()
+    {
+        var parts = await _db.Parts
+            .Where(p => p.IsActive)
+            .Include(p => p.VatRate)
+            .ToListAsync();
+        
+        return parts.Select(_mapper.ToPartListItemViewModel).ToList();
+    }
+
+    public async Task CreatePartAsync(CreatePartViewModel createPartViewModel)
+    {
+        var part = _mapper.ToPart(createPartViewModel);
+        await _db.Parts.AddAsync(part);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task SoftDeletePartAsync(int id)
+    {
+        var part = await _db.Parts.FindAsync(id);
+        if (part is not null)
+        {
+            part.IsActive = false;
+            await _db.SaveChangesAsync();
+        }
+    }
+}
---
+++ CarWorkshopManager/Services/Interfaces/IPartService.cs
@@ -0,0 +1,11 @@
+﻿using CarWorkshopManager.Models.Domain;
+using CarWorkshopManager.ViewModels.Part;
+
+namespace CarWorkshopManager.Services.Interfaces;
+
+public interface IPartService
+{
+    Task<List<PartListItemViewModel>> GetAllPartsAsync();
+    Task CreatePartAsync(CreatePartViewModel createPartViewModel);
+    Task SoftDeletePartAsync(int id);
+}
