114dd18f84b9d76bf5d743d090c2e8a1e39345bf
Grzegorz Rzepiela <grzegorzrzepiela5@gmail.com>
2025-06-11 11:43:24 +0200

    [US6] Add static class with order statuses constants

---
+++ CarWorkshopManager/Constants/OrderStatuses.cs
@@ -0,0 +1,10 @@
+﻿namespace CarWorkshopManager.Constants;
+
+public static class OrderStatuses
+{
+    public const string New = "New";
+    public const string InProgress = "InProgress";
+    public const string Completed = "Completed";
+    
+    public static readonly string[] AllStatuses = { New, InProgress, Completed };
+}
