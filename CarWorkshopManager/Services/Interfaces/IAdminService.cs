3f72cb0d173a0d4d3d343cd726a714340794c85e
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-11 21:42:51 +0200

    Edit update method in AdminService + fix issue with blank username

--- CarWorkshopManager/Services/Interfaces/IAdminService.cs
+++ CarWorkshopManager/Services/Interfaces/IAdminService.cs
@@ -1,4 +1,6 @@
-﻿using CarWorkshopManager.ViewModels.Admin;
+﻿using CarWorkshopManager.Models.Identity;
+using CarWorkshopManager.ViewModels.Admin;
+using Microsoft.AspNetCore.Identity;
 
 namespace CarWorkshopManager.Services.Interfaces;
 
@@ -6,4 +8,7 @@ public interface IAdminService
 {
     Task<List<UserListItemViewModel>> GetAllUsersAsync();
     Task<bool> ChangeUserRoleAsync(string userId, string newRole);
+    Task<IdentityResult> DeleteUserAsync(string userId);
+    Task<ApplicationUser?> GetUserByIdAsync(string userId); 
+    Task<IdentityResult> UpdateUserAsync(ApplicationUser user);
 }
