423c8acdcdd3302d12756a2f9126db19c8bb3d2e
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-11 22:29:22 +0200

    Add mappers for Part model

---
+++ CarWorkshopManager/Mappers/PartMapper.cs
@@ -0,0 +1,15 @@
+﻿using CarWorkshopManager.Models.Domain;
+using CarWorkshopManager.ViewModels.Part;
+using Riok.Mapperly.Abstractions;
+
+namespace CarWorkshopManager.Mappers;
+
+[Mapper(RequiredMappingStrategy = RequiredMappingStrategy.None)]
+public partial class PartMapper
+{
+    public partial Part ToPart(CreatePartViewModel createPartViewModel);
+    
+    [MapperIgnoreSource(nameof(Part.VatRate))] 
+    [MapperIgnoreSource(nameof(Part.UsedParts))] 
+    public partial PartListItemViewModel ToPartListItemViewModel(Part part);
+}
