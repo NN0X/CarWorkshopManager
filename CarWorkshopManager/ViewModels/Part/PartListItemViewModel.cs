988cced4a2d3694ef6e7e3cbe541ac90242b13f7
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-11 22:28:58 +0200

    Add Part viewmodels

---
+++ CarWorkshopManager/ViewModels/Part/PartListItemViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CarWorkshopManager.ViewModels.Part;
+
+public class PartListItemViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal UnitPriceNet { get; set; }
+    public string VatRateCode { get; set; } = string.Empty;
+}
