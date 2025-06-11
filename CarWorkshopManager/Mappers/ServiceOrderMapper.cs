327a6cc935bee1666c6322ba95cf7f6e2a87537a
Grzegorz Rzepiela <grzegorzrzepiela5@gmail.com>
2025-06-11 18:57:47 +0200

    [US6] Add ServiceOrder mapper

---
+++ CarWorkshopManager/Mappers/ServiceOrderMapper.cs
@@ -0,0 +1,11 @@
+﻿using CarWorkshopManager.Models.Domain;
+using CarWorkshopManager.ViewModels.ServiceOrder;
+using Riok.Mapperly.Abstractions;
+
+namespace CarWorkshopManager.Mappers;
+
+[Mapper(RequiredMappingStrategy = RequiredMappingStrategy.None)]
+public partial class ServiceOrderMapper
+{
+    public partial ServiceOrder ToServiceOrder(CreateServiceOrderViewModel model);
+}
