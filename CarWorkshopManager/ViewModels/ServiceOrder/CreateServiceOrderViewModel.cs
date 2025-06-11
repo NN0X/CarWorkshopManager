b3704b0e08307605c6e84e23cc804c9a64026341
Grzegorz Rzepiela <grzegorzrzepiela5@gmail.com>
2025-06-11 18:57:00 +0200

    [US6] Create CreateServiceOrder viewmodel

---
+++ CarWorkshopManager/ViewModels/ServiceOrder/CreateServiceOrderViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CarWorkshopManager.ViewModels.Vehicle;
+
+namespace CarWorkshopManager.ViewModels.ServiceOrder;
+
+public class CreateServiceOrderViewModel
+{
+    [Required]
+    public int VehicleId { get; set; }
+    
+    [Required]
+    [MaxLength(1000)]
+    public string Description { get; set; } = String.Empty;
+
+    public List<VehicleListItemViewModel> Vehicles { get; set; } = new();
+}
