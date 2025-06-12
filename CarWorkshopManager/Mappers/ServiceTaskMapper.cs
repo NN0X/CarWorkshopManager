f4581c1f2f2023b630ebacaba3b539e8d14b2688
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-12 11:23:21 +0200

    Add&update mappers

---
+++ CarWorkshopManager/Mappers/ServiceTaskMapper.cs
@@ -0,0 +1,19 @@
+﻿using CarWorkshopManager.Models.Domain;
+using CarWorkshopManager.Models.Identity;
+using CarWorkshopManager.ViewModels.ServiceTask;
+using Riok.Mapperly.Abstractions;
+
+namespace CarWorkshopManager.Mappers;
+
+[Mapper(RequiredMappingStrategy = RequiredMappingStrategy.None)]
+public partial class ServiceTaskMapper
+{
+    public partial ServiceTask ToServiceTask(ServiceTaskFormViewModel vm);
+    
+    [MapProperty(nameof(ServiceTask.UsedParts), nameof(ServiceTaskListItemViewModel.Parts))]
+    [MapProperty(nameof(ServiceTask.Mechanics), nameof(ServiceTaskListItemViewModel.Mechanics))]
+    public partial ServiceTaskListItemViewModel ToServiceTaskListItemViewModel(ServiceTask task);
+    
+    private static List<string> MapMechanics(ICollection<ApplicationUser> mechanics)
+        => mechanics.Select(m => $"{m.FirstName} {m.LastName}").ToList();
+}
---
+++ CarWorkshopManager/Mappers/UsedPartMapper.cs
@@ -0,0 +1,14 @@
+﻿using CarWorkshopManager.Models.Domain;
+using CarWorkshopManager.ViewModels.UsedPart;
+using Riok.Mapperly.Abstractions;
+
+namespace CarWorkshopManager.Mappers;
+
+[Mapper(RequiredMappingStrategy = RequiredMappingStrategy.None)]
+public partial class UsedPartMapper
+{
+    public partial UsedPart ToUsedPart(UsedPartFormViewModel vm);
+    
+    [MapProperty(nameof(UsedPart.Part.Name), nameof(UsedPartListItemViewModel.PartName))]
+    public partial UsedPartListItemViewModel ToUsedPartListItem(UsedPart usedPart);
+}
