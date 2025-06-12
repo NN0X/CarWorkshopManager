580d00bc2a18ea7ec22532ff1f3feaa9286c24a5
nox <nox@noxsoftware.pl>
2025-06-12 21:22:08 +0200

    done

---
+++ CarWorkshopManager/Services/Implementations/SendGridEmailWithAttachmentSender.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Threading.Tasks;
+using CarWorkshopManager.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace CarWorkshopManager.Services.Implementations
+{
+    public class SendGridEmailWithAttachmentSender : IEmailWithAttachmentSender
+    {
+        private readonly string _apiKey;
+        private readonly EmailAddress _sender;
+
+        public SendGridEmailWithAttachmentSender(IConfiguration configuration)
+        {
+            _apiKey = configuration["SendGrid:ApiKey"]
+                ?? throw new InvalidOperationException("SendGrid API Key is missing");
+            var senderEmail = configuration["SendGrid:SenderEmail"]
+                ?? throw new InvalidOperationException("Sender Email is missing");
+            var senderName = configuration["SendGrid:SenderName"]
+                ?? throw new InvalidOperationException("Sender Name is missing");
+            _sender = new EmailAddress(senderEmail, senderName);
+        }
+
+        public async Task SendEmailAsync(
+            string email,
+            string subject,
+            string htmlMessage,
+            byte[] attachmentBytes,
+            string attachmentFilename,
+            string attachmentType)
+        {
+            var client = new SendGridClient(_apiKey);
+            var msg = MailHelper.CreateSingleEmail(
+                _sender,
+                new EmailAddress(email),
+                subject,
+                plainTextContent: null,
+                htmlContent: htmlMessage);
+
+            var base64 = Convert.ToBase64String(attachmentBytes);
+            msg.AddAttachment(attachmentFilename, base64, attachmentType);
+
+            await client.SendEmailAsync(msg);
+        }
+    }
+}
