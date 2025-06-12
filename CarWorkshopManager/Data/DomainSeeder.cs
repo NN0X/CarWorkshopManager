3021aee8fc657da847f3d64719c7be39fcee95d6
nox <nox@noxsoftware.pl>
2025-06-12 12:14:12 +0200

    domain seeder vat fix

--- CarWorkshopManager/Data/DomainSeeder.cs
+++ CarWorkshopManager/Data/DomainSeeder.cs
@@ -1,5 +1,6 @@
 ﻿using CarWorkshopManager.Constants;
 using CarWorkshopManager.Models.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarWorkshopManager.Data;
 
@@ -7,37 +8,71 @@ public static class DomainSeeder
 {
     public static async Task SeedAsync(ApplicationDbContext db)
     {
-        foreach (var statusName in OrderStatuses.AllStatuses)
+        var existingStatusNames = await db.OrderStatuses
+                                          .Select(s => s.Name)
+                                          .ToListAsync();
+        var statusesToAdd = OrderStatuses.AllStatuses
+                                         .Where(name => !existingStatusNames.Contains(name))
+                                         .Select(name => new OrderStatus { Name = name })
+                                         .ToList();
+        if (statusesToAdd.Any())
         {
-            if (!db.OrderStatuses.Any(s => s.Name == statusName))
-            {
-                db.OrderStatuses.Add(new OrderStatus { Name = statusName });
-            }
+            db.OrderStatuses.AddRange(statusesToAdd);
         }
 
-        foreach (var (rate, code) in VatRates.AllVatRates)
+        var existingVatCodes = await db.VatRates
+                                       .Select(v => v.Code)
+                                       .ToListAsync();
+        var vatToAdd = VatRates.AllVatRates
+                               .Where(tuple => !existingVatCodes.Contains(tuple.Code))
+                               .Select(tuple => new VatRate
+                               {
+                                   Rate = tuple.Rate,
+                                   Code = tuple.Code,
+                                   ValidFrom = DateOnly.FromDateTime(DateTime.UtcNow)
+                               })
+                               .ToList();
+        if (vatToAdd.Any())
         {
-            if (!db.VatRates.Any(v => v.Rate == rate && v.Code == code))
-            {
-                db.VatRates.Add(new VatRate { Rate = rate, Code = code, ValidFrom = DateOnly.FromDateTime(DateTime.UtcNow) });
-            }
+            db.VatRates.AddRange(vatToAdd);
         }
-        
-        var defaultVat23 = db.VatRates.First(v => v.Code == VatRates.Vat23);
-        foreach (var (hourRate, name) in WorkRates.AllWorkRates)
-        {
-            if (!db.WorkRates.Any(w => w.Name == name))
-            {
-                db.WorkRates.Add(new WorkRate
-                {
-                    Name = name,
-                    HourRateNet = hourRate,
-                    VatRateId = defaultVat23.Id,
-                    ValidFrom = DateOnly.FromDateTime(DateTime.UtcNow)
-                });
-            }
-        }
-        
+
         await db.SaveChangesAsync();
+
+        var defaultVat23 = await db.VatRates
+                                  .FirstOrDefaultAsync(v => v.Code == VatRates.Vat23);
+        if (defaultVat23 == null)
+        {
+            var tuple23 = VatRates.AllVatRates
+                                  .FirstOrDefault(t => t.Code == VatRates.Vat23);
+            decimal rate23 = tuple23 != default ? tuple23.Rate : 0.23M;
+            defaultVat23 = new VatRate
+            {
+                Code = VatRates.Vat23,
+                Rate = rate23,
+                ValidFrom = DateOnly.FromDateTime(DateTime.UtcNow)
+            };
+            db.VatRates.Add(defaultVat23);
+            await db.SaveChangesAsync();
+        }
+
+        var existingWorkNames = await db.WorkRates
+                                        .Select(w => w.Name)
+                                        .ToListAsync();
+        var workRatesToAdd = WorkRates.AllWorkRates
+                                      .Where(tuple => !existingWorkNames.Contains(tuple.Name))
+                                      .Select(tuple => new WorkRate
+                                      {
+                                          Name = tuple.Name,
+                                          HourRateNet = tuple.HourRateNet,
+                                          VatRate = defaultVat23,
+                                          ValidFrom = DateOnly.FromDateTime(DateTime.UtcNow)
+                                      })
+                                      .ToList();
+        if (workRatesToAdd.Any())
+        {
+            db.WorkRates.AddRange(workRatesToAdd);
+            await db.SaveChangesAsync();
+        }
     }
 }
