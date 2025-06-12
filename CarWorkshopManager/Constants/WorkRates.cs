b020679748d0bb5b552fa03db1cf20cd8c2995ea
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-12 11:24:29 +0200

    Seed WorkRates in database

---
+++ CarWorkshopManager/Constants/WorkRates.cs
@@ -0,0 +1,14 @@
+﻿namespace CarWorkshopManager.Constants
+{
+    public static class WorkRates
+    {
+        public const string Standard = "Standard";
+        public const string Diagnostics = "Diagnostics";
+
+        public static readonly (decimal HourRateNet, string Name)[] AllWorkRates =
+        {
+            (120m, Standard),
+            (150m, Diagnostics),
+        };
+    }
+}
