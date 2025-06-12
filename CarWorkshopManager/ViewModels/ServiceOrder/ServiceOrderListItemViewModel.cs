9186e3ad2acc5e799a1fa4567e7787b40f001f15
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-06-12 11:22:29 +0200

    Add&update viewmodels

---
+++ CarWorkshopManager/ViewModels/ServiceOrder/ServiceOrderListItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CarWorkshopManager.ViewModels.ServiceOrder;
+
+public class ServiceOrderListItemViewModel
+{
+    public int Id { get; set; }
+    public string OrderNumber { get; set; } = string.Empty;
+    public DateTime OpenedAt { get; set; }
+    public DateTime ClosedAt { get; set; }
+    public string CustomerName { get; set; } = string.Empty;
+    public string RegistrationNumber { get; set; } = string.Empty;
+    public string StatusName { get; set; } = string.Empty;
+    public decimal TotalNet { get; set; }
+    public decimal TotalVat { get; set; }
+}
---
+++ CarWorkshopManager/ViewModels/ServiceTask/ServiceTaskFormViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarWorkshopManager.ViewModels.ServiceTask;
+
+public class ServiceTaskFormViewModel
+{
+    [Required]
+    public int ServiceOrderId { get; set; }
+
+    [Required, MaxLength(200)]
+    [Display(Name = "Opis czynności")]
+    public string Description { get; set; } = string.Empty;
+
+    [Required, Range(0.1, 1000)]
+    [Display(Name = "Czas pracy [h]")]
+    public decimal WorkHours { get; set; }
+
+    [Required]
+    [Display(Name = "Stawka robocizny")]
+    public int WorkRateId { get; set; }
+
+    [Required(ErrorMessage = "Wybierz przynajmniej jednego mechanika")]
+    [Display(Name = "Mechanicy")]
+    public List<string> MechanicsIds { get; set; } = new();
+}
